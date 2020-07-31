3fb94c4529e9778b27f47aa931d29b53d90a5acb
fwh <15279279713@163.com>
2020-07-31 10:05:14 +0800

    2020-07-30

---
+++ function/add.js
@@ -0,0 +1,8 @@
+function add(arr) {
+    let count=0
+    arr.forEach(item=>{
+        count+=item
+    })
+    console.log(count);
+}
+add([1,2,3])
---
+++ 运算符/|| 或.js
@@ -0,0 +1,6 @@
+let a=3
+if(a==1||a==2){
+    console.log('123')
+}else{
+    console.log('你错了')
+}
