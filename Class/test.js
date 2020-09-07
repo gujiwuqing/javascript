3978a8e79ef6d4e18039991ed078c93c9cd9dc88
DESKTOP-O8DL8KK\1 <15279279713@163.com>
2020-09-07 11:50:40 +0800

    'class'

---
+++ Class/test.js
@@ -0,0 +1,9 @@
+class Person {
+    constructor(name,age,sex) {
+        this.name = name
+        this.age = age
+        this.sex = sex
+    }
+}
+const xiaoming = new Person('xioming',18,'男')
+console.log(xiaoming);
--- 运算符/|| 或.js
+++
@@ -1,6 +0,0 @@
-let a=3
-if(a==1||a==2){
-    console.log('123')
-}else{
-    console.log('你错了')
-}
