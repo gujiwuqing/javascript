b030794752b09d175f5ebe648646990356b9448c
gujiwuqing <15279279713@163.com>
2020-09-11 23:33:49 +0800

    2020/9/11

--- Class/test.js
+++ Class/test.js
@@ -7,3 +7,14 @@ class Person {
 }
 const xiaoming = new Person('xioming',18,'男')
 console.log(xiaoming);
+
+class student extends Person{
+    constructor(name) {
+        super(name);
+        this.name = name
+        this.age = 17
+        this.sex='女'
+    }
+}
+const le = new student('test')
+console.log(le);
--- 拷贝/深拷贝.js
+++ 拷贝/深拷贝.js
@@ -1,26 +1,28 @@
 const obj = {
-    a:1,
-    b:[1,2,3,4],
-    c:{
-        name:'test',
-        age:12
+    a: 1,
+    b: [1, 2, 3, 4],
+    c: {
+        name: 'test',
+        age: 12
     },
-    d:function test() {
+    d: function test() {
         console.log(123);
     }
 }
 const o = JSON.parse(JSON.stringify(obj))
 console.log(o);
 
+
+//深拷贝简易版
 function deepCopy(object) {
-    if (typeof object!='object') return
-    let newObj = object instanceof Array?[]:{}
+    if (typeof object != 'object') return
+    let newObject = Object.prototype.toString.call(object)==="[object Object]"?{}:[]
     for (let key in object) {
         if (object.hasOwnProperty(key)){
-            newObj[key] = typeof object[key]==='object'?deepCopy(object[key]):object[key]
+            newObject[key] = Object.prototype.toString.call(object[key])==="[object Object]"?deepCopy(object[key]):object[key]
         }
     }
-    return newObj
+    return newObject
 }
 
 console.log(deepCopy(obj));
